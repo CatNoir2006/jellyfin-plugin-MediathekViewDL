8a75012c46f903722f404c9bd5897fbc116cea70
CatNoir2006 <93994507+CatNoir2006@users.noreply.github.com>
2025-12-19 16:21:08 +0100

    feat: Add title and language to download history and update tooling  - Added migration `AddTitleAndLanguageToDownloadHistory`.  - Updated `.editorconfig` to treat the `Migrations` folder as generated code to bypass StyleCop rules.  - Improved the Web UI to display the download title instead of the filename when available.

--- Jellyfin.Plugin.MediathekViewDL/Data/Migrations/20251219144756_AddTitleAndLanguageToDownloadHistory.Designer.cs
+++ Jellyfin.Plugin.MediathekViewDL/Data/Migrations/20251219144756_AddTitleAndLanguageToDownloadHistory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Jellyfin.Plugin.MediathekViewDL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jellyfin.Plugin.MediathekViewDL.Data.Migrations
 {
     [DbContext(typeof(MediathekViewDlDbContext))]
-    partial class MediathekViewDlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251219144756_AddTitleAndLanguageToDownloadHistory")]
+    partial class AddTitleAndLanguageToDownloadHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
