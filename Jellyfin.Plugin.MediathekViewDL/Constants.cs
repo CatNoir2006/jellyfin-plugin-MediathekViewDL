f246cc1cabd258bc776c8192381467568005e84b
CatNoir2006 <93994507+CatNoir2006@users.noreply.github.com>
2026-01-05 15:47:48 +0100

    Implement TempFileCleanup scheduled task

---
+++ Jellyfin.Plugin.MediathekViewDL/Constants.cs
@@ -0,0 +1,12 @@
+namespace Jellyfin.Plugin.MediathekViewDL;
+
+/// <summary>
+/// Contains global constants for the plugin.
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// The Category for the Scheduled Tasks of this Plugin.
+    /// </summary>
+    public const string SchedTaskCat = "Mediathek Downloader";
+}
